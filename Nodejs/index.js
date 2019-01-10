30b5a98043a66b16532f9cffc9c9cd29b29d035f
yogathanh99 <yogathanh98@gmail.com>
2019-01-10 14:49:56 +0700

    [UPDATE] index.js

--- Nodejs/index.js
+++ Nodejs/index.js
@@ -16,11 +16,9 @@ app.use(bodyParser.json()); // for parsing application/json
 app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
 app.use(cookieParser(process.env.SESSION_SECRET))
 
-
 //Set-up for Pug file
 app.set('view engine', 'pug')
 app.set('views','./views')
-
 //Using static path (images, css, javascript)
 app.use(express.static('public'))
 
