41db2613ea1da1ff438ca048145f4df374940cf7
yogathanh99 <yogathanh98@gmail.com>
2019-01-25 14:27:04 +0700

    [UPDATE] Validate for user

--- Nodejs/validate/userValidate.js
+++ Nodejs/validate/userValidate.js
@@ -1,4 +1,8 @@
 module.exports.validation = (req, res, next) => {
+  const patterns = {
+    phone: /^\d{11}$/,
+    password: /^[\w@-]{8,20}$/
+  }
   let errors = []
 
   if (!req.body.name) {
@@ -6,6 +10,12 @@ module.exports.validation = (req, res, next) => {
   }
   if (!req.body.phone) {
     errors.push('Phone is required')
+  }
+  if (!patterns.phone.test(req.body.phone)) {
+    errors.push('Phone must have 11 numbers')
+  }
+  if (!patterns.password.test(req.body.password)) {
+    errors.push('Password must alphanumeric (@, _ and - are also allowed) and be 8 - 20 characters')
   }
 
   if (errors.length) {
